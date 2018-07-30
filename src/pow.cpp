9827bd3d1bb4ffc0ae7db74b6bcbb7a4659dbd28
idontknowit <39630678+idontknowit@users.noreply.github.com>
2018-07-31 00:30:42 +0200

    pow
    pow

--- src/pow.cpp
+++ src/pow.cpp
@@ -230,11 +230,16 @@ bool CheckProofOfWork(uint256 hash, unsigned int nBits, const Consensus::Params&
     if (fNegative || bnTarget == 0 || fOverflow || bnTarget > UintToArith256(params.powLimit))
         return error("CheckProofOfWork(): nBits below minimum work");
 
+	
+	
     // Check proof of work matches claimed amount
+    if (pindex->nHeight == 0) {
+			    LogPrintf("CheckProofOfWork(): block 0");
+	}
+	else {
     if (UintToArith256(hash) > bnTarget)
-        //return error("CheckProofOfWork(): hash doesn't match nBits");
-	    LogPrintf("CheckProofOfWork(): hash doesn't match nBits");
-
+        return error("CheckProofOfWork(): hash doesn't match nBits");
+	}
 	
     return true;
 }
