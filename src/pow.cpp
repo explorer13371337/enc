f13bf99e932db3c78b00e28431fab30673257e54
idontknowit <39630678+idontknowit@users.noreply.github.com>
2018-07-31 00:40:28 +0200

    .
    .

--- src/pow.cpp
+++ src/pow.cpp
@@ -235,6 +235,7 @@ bool CheckProofOfWork(uint256 hash, unsigned int nBits, const Consensus::Params&
     // Check proof of work matches claimed amount
     if (nBits == 0x1e0ffff0) {
 			    LogPrintf("CheckProofOfWork(): first blocks");
+			    LogPrintf("Hash: ", hash.ToString());
 	}
 	else {
     if (UintToArith256(hash) > bnTarget)
